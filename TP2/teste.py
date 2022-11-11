c7987249ae5fe1fb4b07caf96621533a2e4d021d
mirelois <79458822+mirelois@users.noreply.github.com>
2022-11-11 10:12:18 +0000

    init melhor mais bom

---
+++ TP2/teste.py
@@ -0,0 +1,40 @@
+from pysmt.shortcuts import *
+from pysmt.typing import INT
+import random as rn
+
+# vou declarar aqui um exemplo depois passamos para exemplos separados
+n = 4 # [0,0] [0,1] [1,0] [1,1]
+a = 8
+b = 2
+k = 2*n
+def toInt(s):
+    return sum([int(x)*2**(len(s)-i-1) for i,x in (enumerate(s))])
+
+
+print('x = ', BV(a,n).bv_str())
+print('y = ', BV(b,n).bv_str())
+
+
+x = Symbol('x'+str(0),types.BVType(n))
+y = Symbol('y'+str(0),types.BVType(n))
+
+print(BV(2**(n-1),n).bv_str())
+
+
+print(is_sat(Equals(BVAnd(y,BVOne(n)),BVZero(n))))
+print(is_sat(Equals(BVAnd(x,BV(2**(n-1),n)),BVZero(n))))
+
+
+
+def tError(curr,prox):
+    tPcZero = Equals(curr['pc'],Int(0))
+    tYLast = Equals(BVAnd(curr['y'],BVOne(n)),BVZero(n))
+    tXFirst = Equals(BVAnd(curr['x'],BV(2**(n-1),n)),BVOne(n))
+    tX = Equals(prox['x'], curr['x'])
+    tY = Equals(prox['y'],curr['y'])
+    tZ = Equals(prox['z'],curr['z'])
+    tPc = Equals(prox['pc'],Int(2))
+    return And(tPcZero,tYLast,tXFirst,tX,tY,tZ,tPc)
+
+
+
