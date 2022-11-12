8ae0b73a193c3c43fb24176ad8a3865a0eda3392
mirelois <79458822+mirelois@users.noreply.github.com>
2022-11-12 15:14:22 +0000

    ???????????????????????????????????????????????????????????????????????????????????????????

--- TP2/teste.py
+++ TP2/teste.py
@@ -2,42 +2,19 @@ from pysmt.shortcuts import *
 from pysmt.typing import INT
 import random as rn
 
-def BVLast(n):
-    return BV(2**(n-1),n)
-# vou declarar aqui um exemplo depois passamos para exemplos separados
-n = 5 # [0,0] [0,1] [1,0] [1,1]
-a = 5
-b = 2
-k = 2*n
-def toInt(s):
-    return sum([int(x)*2**(len(s)-i-1) for i,x in (enumerate(s))])
+def PontosProximos(pos):
+    return [(pos[0]+1,pos[1]),(pos[0]-1,pos[1]),(pos[0],pos[1]+1),(pos[0],pos[1]-1),(pos[0]-1,pos[1]-1),(pos[0]+1,pos[1]+1),(pos[0]-1,pos[1]+1),(pos[0]+1, pos[1]-1)]
 
 
-print('x = ', BV(a,n).bv_str())
-print('y = ', BV(b,n).bv_str())
+N = 10
+i,j = 4,4
+
+print(len(list(filter(lambda x: x[0] < N+1 and x[1] < N+1,  PontosProximos((i,j))))))
+
+d = 0
+for k in filter(lambda x: x[0] < N+1 and x[1] < N+1,  PontosProximos((i,j))):
+    d += 1
+print(d)
 
 
-x = Symbol('x'+str(0),types.BVType(n))
-y = Symbol('y'+str(0),types.BVType(n))
-
-print(BV(2**(n-1),n).bv_str())
-
-
-print(is_sat(Equals(BVAnd(y,BVOne(n)),BVZero(n))))
-print(is_sat(Equals(BVAnd(x,BV(2**(n-1),n)),BVZero(n))))
-print(is_sat(And(Equals(x,BV(a,n)), Equals(BVAnd(x,BV(2**(n-1),n)),BVZero(n)))))
-
-
-
-def tError(curr,prox):
-    tPcZero = Equals(curr['pc'],Int(0))
-    tYLast = Equals(BVAnd(curr['y'],BVOne(n)),BVZero(n))
-    tXFirst = Equals(BVAnd(curr['x'],BV(2**(n-1),n)),BVOne(n))
-    tX = Equals(prox['x'], curr['x'])
-    tY = Equals(prox['y'],curr['y'])
-    tZ = Equals(prox['z'],curr['z'])
-    tPc = Equals(prox['pc'],Int(2))
-    return And(tPcZero,tYLast,tXFirst,tX,tY,tZ,tPc)
-
-
-
+print((1,5) < (4,4))
